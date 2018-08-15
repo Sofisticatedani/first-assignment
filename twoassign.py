c8246a211ce2ece0cd37f074eddf70dcef11d2d0
Sofisticatedani <42324586+Sofisticatedani@users.noreply.github.com>
2018-08-15 13:07:57 +0100

    first and second python assignment
    first and second python assignment

---
+++ twoassign.py
@@ -0,0 +1,9 @@
+#request for name
+name = input('What is your name? \n')
+print('Welcome',name)
+#request for age
+age = int(input('Enter your present age number\n'))
+year = 2018 - age
+nYear = year + 100
+print('Dear',name,' you will be 100 years old in the year ',nYear)
+
